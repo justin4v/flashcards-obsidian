00c449eb5643dd9b30de8aa04eba3ab0a5cfc6cc
junjie.fu_U+ <junjie.fu@united-imaging.com>
2023-10-17 10:33:56 +0800

    初始化提交-main

---
+++ src/conf/settings.ts
@@ -0,0 +1,14 @@
+export interface ISettings {
+  contextAwareMode: boolean;
+  sourceSupport: boolean;
+  codeHighlightSupport: boolean;
+  inlineID: boolean;
+  contextSeparator: string;
+  deck: string;
+  folderBasedDeck: boolean;
+  flashcardsTag: string;
+  inlineSeparator: string;
+  inlineSeparatorReverse: string;
+  defaultAnkiTag: string;
+  ankiConnectPermission: boolean;
+}
